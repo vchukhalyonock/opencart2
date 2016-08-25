fe7dd08600066316bc21d342f96af06d49becc0b
Vladimir Chukhaloynock <vchukhalyonock@gmail.com>
2016-08-25 11:16:33 +0300

    Initial Commit

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://opencart/admin/');
+define('HTTP_CATALOG', 'http://opencart/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://opencart/admin/');
+define('HTTPS_CATALOG', 'http://opencart/');
+
+// DIR
+define('DIR_APPLICATION', '/home/mcmxx/www/opencart/admin/');
+define('DIR_SYSTEM', '/home/mcmxx/www/opencart/system/');
+define('DIR_IMAGE', '/home/mcmxx/www/opencart/image/');
+define('DIR_LANGUAGE', '/home/mcmxx/www/opencart/admin/language/');
+define('DIR_TEMPLATE', '/home/mcmxx/www/opencart/admin/view/template/');
+define('DIR_CONFIG', '/home/mcmxx/www/opencart/system/config/');
+define('DIR_CACHE', '/home/mcmxx/www/opencart/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/mcmxx/www/opencart/system/storage/download/');
+define('DIR_LOGS', '/home/mcmxx/www/opencart/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/mcmxx/www/opencart/system/storage/modification/');
+define('DIR_UPLOAD', '/home/mcmxx/www/opencart/system/storage/upload/');
+define('DIR_CATALOG', '/home/mcmxx/www/opencart/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'htpthdfwbz');
+define('DB_DATABASE', 'opencart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://opencart/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://opencart/');
+
+// DIR
+define('DIR_APPLICATION', '/home/mcmxx/www/opencart/catalog/');
+define('DIR_SYSTEM', '/home/mcmxx/www/opencart/system/');
+define('DIR_IMAGE', '/home/mcmxx/www/opencart/image/');
+define('DIR_LANGUAGE', '/home/mcmxx/www/opencart/catalog/language/');
+define('DIR_TEMPLATE', '/home/mcmxx/www/opencart/catalog/view/theme/');
+define('DIR_CONFIG', '/home/mcmxx/www/opencart/system/config/');
+define('DIR_CACHE', '/home/mcmxx/www/opencart/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/mcmxx/www/opencart/system/storage/download/');
+define('DIR_LOGS', '/home/mcmxx/www/opencart/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/mcmxx/www/opencart/system/storage/modification/');
+define('DIR_UPLOAD', '/home/mcmxx/www/opencart/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'htpthdfwbz');
+define('DB_DATABASE', 'opencart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
