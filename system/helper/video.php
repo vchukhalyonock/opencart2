3300e085a5ab4db9c3f091f5439c4ead0cdd2a09
Vladimir Chukhaloynock <vchukhalyonock@gmail.com>
2016-08-25 17:15:14 +0300

    Implement video helper function downloadFromYoutube

--- system/helper/video.php
+++ system/helper/video.php
@@ -31,4 +31,63 @@ if ( !function_exists('getYoutubeVideoInfoContent') ) {
 		return $content;
 	}
 }
+
+
+
+if ( !function_exists('downloadFromYoutube') ) {
+	/**
+	 * @param string $video_id
+	 * @param string $youtube_id
+	 * @param string $video_type
+	 * @return bool|mixed
+	 */
+	function downloadFromYoutube($video_id = '', $youtube_id = '', $video_type = 'video/mp4')
+	{
+		$dataToSend = array();
+		$dataToSend['video_id'] = $video_id;
+		$dataToSend['youtube_id'] = $youtube_id;
+		$dataToSend['video_type'] = $video_type;
+
+		$temp = tmpfile();
+		fwrite($temp, json_encode($dataToSend));
+		fseek($temp, 0);
+
+		// generate request for user api service
+		$request = curl_init();
+
+		curl_setopt_array($request, array(
+			CURLOPT_URL => $api_url . $api_video_path,
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_SSL_VERIFYPEER => false,
+			CURLOPT_PUT => true,
+			CURLOPT_BINARYTRANSFER => true,
+			CURLOPT_UPLOAD => true,
+			CURLOPT_INFILE => $temp,
+			CURLOPT_INFILESIZE, strlen(json_encode($dataToSend)),
+		));
+
+		$response = curl_exec($request);
+		fclose($temp);
+
+		if ( curl_getinfo($request, CURLINFO_HTTP_CODE) === 200 ) {
+
+			$json = json_decode($response, true);
+			curl_close($request);
+
+			if ( $json == null ) {
+				return false;
+			}
+
+			if ( $json['status'] != true ) {
+				return false;
+			}
+
+		} else {
+			curl_close($request);
+			return false;
+		}
+
+		return $json;
+	}
+}
 ?>
