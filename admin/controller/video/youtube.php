46515a79586048559719d9107bd8523f88cd4b9e
Vladimir Chukhaloynock <vchukhalyonock@gmail.com>
2016-09-01 18:42:25 +0300

    Partually implemented video for CLI

--- admin/controller/video/youtube.php
+++ admin/controller/video/youtube.php
@@ -5,7 +5,15 @@
 class ModelVideoYoutube extends Controller {
 	
 	public function download() {
+		$this->load->model('video/channel');
 
+		$allVideos = $this->model_video_channel->getAllVideos(null, null, 'download');
+		if(count($allVideos['result']) == 0)
+			die;
+
+		foreach ($allVideos as $video) {
+			
+		}
 	}
 
 
