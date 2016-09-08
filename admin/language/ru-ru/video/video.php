4c8fe9b232033d7a39114a4977940db7b598ea96
Vladimir Chukhaloynock <vchukhalyonock@gmail.com>
2016-09-08 10:47:29 +0300

    Add video lang file gor russian

---
+++ admin/language/ru-ru/video/video.php
@@ -0,0 +1,67 @@
+<?php
+// Heading
+$_['heading_title']     = 'Видео';
+$_['groups_heading_title'] = 'Группы';
+
+
+// Text
+$_['text_success']          = 'Видео успешно изменено!';
+$_['text_list']             = 'Видео';
+$_['text_groups_list']      = 'Группы видео';
+$_['text_add']              = 'Добавить видео';
+$_['text_edit']             = 'Изменить видео';
+$_['text_group_add']		= 'Создать группу';
+$_['text_group_edit']		= 'Изменить группу';
+
+
+//Column
+$_['column_id']				= '№';
+$_['column_email']			= 'Email пользователя';
+$_['column_name']			= 'Наименование';
+$_['column_status']			= 'Статус';
+$_['column_featured']		= 'На главную';
+$_['column_customer_link']	= 'Источник';
+$_['column_channel_id']		= 'На канале';
+$_['column_actions']		= 'Действия';
+$_['column_associated']		= 'Ассоциировано';
+
+
+//Entry
+$_['entry_status']			= 'Статус';
+$_['entry_name']			= 'Наименование';
+$_['entry_description']		= 'Описание';
+$_['entry_customer_link']	= 'Источник';
+$_['entry_channel_link']	= 'На канале';
+$_['entry_featured']		= 'На главную';
+$_['entry_search']			= 'Поиск';
+
+
+//Statuses
+$_['status_new']			= 'Новое';
+$_['status_download']		= 'К загрузке';
+$_['status_downloaded']		= 'Загружено';
+$_['status_upload']			= 'К выгрузке';
+$_['status_not_ready']		= 'Не готово';
+$_['status_ready']			= 'Готово';
+$_['status_error_upload']	= 'Ошибка выгрузки';
+$_['status_error_download']	= 'Ошибка загрузки';
+
+//Featured
+$_['text_featured'] = 'На главной';
+$_['text_not_featured'] = 'Не на главной';
+
+//Associated
+$_['text_associated'] = 'В группе';
+$_['text_not_associated'] = 'Не в группе';
+
+//Buttons
+$_['button_get_content'] = 'Получить данные видео';
+$_['button_get_groups'] = 'Группы';
+
+
+//Errors
+$_['error_warning']			= 'Внимание: Проверьте данные на предмет ошибок!';
+$_['error_permission']		= 'Внимание: У вас нет прав на редактирование видео!';
+$_['error_customer_link']	= 'Источник должен быть ссылкой и быть не более 255 символов!';
+$_['error_customer_link_exist']	= 'Такой источник уже существует!';
+$_['error_group_name']	= 'Название должно быть длиной не менее 1го и не более 255 символов!';
